4a0d6c9bee17b23796cedde1f364dba85e0b877a
YOUSEF AMRO <YOUSEF AMRO>
2017-02-13 02:24:10 +0200

    movie of the jordan

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity55.java
@@ -0,0 +1,116 @@
+package com.example.user.myapplication;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity55 extends AppCompatActivity {
+
+
+    private VideoView videoView;
+    private int position = 0;
+    private MediaController mediaController;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main55);
+
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        // Set the media controller buttons
+        if (mediaController == null) {
+            mediaController = new MediaController(MainActivity55.this);
+
+            // Set the videoView that acts as the anchor for the MediaController.
+            mediaController.setAnchorView(videoView);
+
+
+            // Set MediaController for VideoView
+            videoView.setMediaController(mediaController);
+        }
+
+
+        try {
+            // ID of video file.
+            int id = this.getRawResIdByName("m1");
+            videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + id));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoView.requestFocus();
+
+
+        // When the video file ready for playback.
+        videoView.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+
+
+                videoView.seekTo(position);
+                if (position == 0) {
+                    videoView.start();
+                }
+
+                // When video Screen change size.
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+                        // Re-Set the videoView that acts as the anchor for the MediaController
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+
+    }
+
+    // Find ID corresponding to the name of the resource (in the directory raw).
+    public int getRawResIdByName(String resName) {
+        String pkgName = this.getPackageName();
+        // Return 0 if not found.
+        int resID = this.getResources().getIdentifier(resName, "raw", pkgName);
+        Log.i("AndroidVideoView", "Res Name: " + resName + "==> Res ID = " + resID);
+        return resID;
+    }
+
+
+    // When you change direction of phone, this method will be called.
+    // It store the state of video (Current position)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Store current position.
+        savedInstanceState.putInt("CurrentPosition", videoView.getCurrentPosition());
+        videoView.pause();
+    }
+
+
+    // After rotating the phone. This method is called.
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get saved position.
+        position = savedInstanceState.getInt("CurrentPosition");
+        videoView.seekTo(position);
+    }
+
+}
